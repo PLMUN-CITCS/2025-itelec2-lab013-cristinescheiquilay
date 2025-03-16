e5684b11e9aa3e111bf74f18dc91732d1acc9e46
cristinescheiquilay <scheiyaliuq@gmail.com>
2025-03-16 14:13:21 +0800

    Submitting Python Week 04 - Laboratory # 13

---
+++ for_loop_break_continue.py
@@ -0,0 +1,7 @@
+numbers = list(range(1, 11))
+for num in numbers: 11
+    if num == 3:
+        continue  # Skip the rest of this iteration
+    if num == 7:
+        break  # Exit the loop completely 
+  print(num)
